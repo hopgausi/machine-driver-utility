3db2dffb4e1595b60062cae21d5ea3852aeaf878
Hopson Gausi <hopgausi20@gmail.com>
2022-08-26 11:16:54 +0200

    remove test

--- test.js
+++
@@ -1,8 +0,0 @@
-var utils = require('./index');
-
-lisBaseUrl = "http://localhost:8000/api/update_result"
-
-utils.buildUrl(lisBaseUrl,220023,45,'67.995');
-console.log(utils.urls)
-utils.sendDataToIBLIS(utils.urls, 'user', 'password');
-
